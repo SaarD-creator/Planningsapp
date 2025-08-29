f76d11fa5971e66964494e040500cebbf8a96292
Saar-creator <dayerssaar@gmail.com>
2025-08-29 22:57:33 +0200

    Update Planningsapp.py

--- Planningsapp.py
+++ Planningsapp.py
@@ -7,6 +7,7 @@ from openpyxl.styles import Font, Alignment, PatternFill, Border, Side
 from openpyxl.utils import get_column_letter
 from io import BytesIO
 import datetime
+from openpyxl.styles import Font
 
 
 # -----------------------------
