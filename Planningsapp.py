140f1e5ad76082fc9f838e33e6e81264b2c3c223
Saar-creator <dayerssaar@gmail.com>
2025-08-31 10:38:20 +0200

    Update Planningsapp.py

--- Planningsapp.py
+++ Planningsapp.py
@@ -3,6 +3,8 @@ import copy
 import random
 from collections import defaultdict
 from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Font, Alignment, PatternFill, Border, Side
+from openpyxl.utils import get_column_letter
 from io import BytesIO
 import datetime
 
