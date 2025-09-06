8a763a8b009690d25cd945f95a28cf8f8f79f8ee
Saar-creator <dayerssaar@gmail.com>
2025-09-06 21:01:19 +0200

    Update Planningsapp.py

--- Planningsapp.py
+++ Planningsapp.py
@@ -1,7 +1,7 @@
 #red spots, geen (ithink) extra's, geen lange blokken, wel nog extra bij max van 4 uur
 
 #perfectttt, enkel nog probleempje als persoon met meeste attracties iets niet kan
-
+kak
 # we zien de red spots al, gwn nog teveel bij extra
 
 
