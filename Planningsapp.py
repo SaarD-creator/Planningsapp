4930c7cb8238d4d0e1487ec4ed168d4038320226
Saar-creator <dayerssaar@gmail.com>
2025-09-14 18:13:22 +0200

    Refactor block collection logic for time ranges

--- Planningsapp.py
+++ Planningsapp.py
@@ -659,6 +659,14 @@ for row in ws_out.iter_rows(min_row=2, values_only=True):
 
 
 
+
+
+
+
+
+
+
+
 #DEEL 2
 #oooooooooooooooooooo
 #oooooooooooooooooooo
@@ -1083,29 +1091,29 @@ def plaats_student(student, harde_mode=False):
         eerste_uur = uur_col_pairs[0][0]
         grens_uur_2_5 = eerste_uur + 2
         grens_uur_3 = eerste_uur + 3
-        # Verzamel blokken in de eerste 2,5 uur, markeer het laatste halfuur van de eerste 3 uur apart
+        # Blokken volledig binnen de eerste 2,5 uur
         blokken_links = []
-        blokken_links_laatste_halfuur_3u = []
+        blokken_links_laatste_uur_3u = []
         blokken_rechts = []
-        blokken_rechts_laatste_halfuur_3u = []
+        blokken_rechts_laatste_uur_3u = []
         for i in range(len(uur_col_pairs)-1):
             uur1, col1 = uur_col_pairs[i]
             uur2, col2 = uur_col_pairs[i+1]
             if col2 == col1 + 1:
-                # Normale blokken in eerste 2,5 uur
-                if uur1 >= eerste_uur and uur2 < grens_uur_2_5:
+                # Blokken volledig binnen de eerste 2,5 uur
+                if uur1 >= eerste_uur and uur2 <= grens_uur_2_5:
                     blokken_links.append((i, uur1, col1, uur2, col2))
-                # Blokken die eindigen in het laatste halfuur van de eerste 3 uur
-                elif uur2 == grens_uur_3:
-                    blokken_links_laatste_halfuur_3u.append((i, uur1, col1, uur2, col2))
+                # Blokken die (deels) in het laatste uur van de eerste 3 uur vallen
+                elif (uur1 >= grens_uur_2_5 or uur2 > grens_uur_2_5) and uur2 <= grens_uur_3:
+                    blokken_links_laatste_uur_3u.append((i, uur1, col1, uur2, col2))
         for i in reversed(range(len(uur_col_pairs)-1)):
             uur1, col1 = uur_col_pairs[i]
             uur2, col2 = uur_col_pairs[i+1]
             if col2 == col1 + 1:
-                if uur1 >= eerste_uur and uur2 < grens_uur_2_5:
+                if uur1 >= eerste_uur and uur2 <= grens_uur_2_5:
                     blokken_rechts.append((i, uur1, col1, uur2, col2))
-                elif uur2 == grens_uur_3:
-                    blokken_rechts_laatste_halfuur_3u.append((i, uur1, col1, uur2, col2))
+                elif (uur1 >= grens_uur_2_5 or uur2 > grens_uur_2_5) and uur2 <= grens_uur_3:
+                    blokken_rechts_laatste_uur_3u.append((i, uur1, col1, uur2, col2))
         lange_werkers_lijst = getattr(plaats_student, "_lange_werkers_lijst", None)
         if lange_werkers_lijst is None:
             lange_werkers_lijst = [s["naam"] for s in studenten if (student_totalen.get(s["naam"], 0) > 6 or ("-18" in str(s["naam"]) and student_totalen.get(s["naam"], 0) > 0)) and s["naam"] not in [pv["naam"] for pv in selected]]
@@ -1115,12 +1123,12 @@ def plaats_student(student, harde_mode=False):
             lange_pauze_opties = blokken_links
         else:
             lange_pauze_opties = blokken_rechts
-        # Als er te weinig blokken zijn, voeg blokken toe uit de eerste 3 uur, maar laatste halfuur weer apart
+        # Als er te weinig blokken zijn, voeg blokken toe uit het laatste uur van de eerste 3 uur
         if len(lange_pauze_opties) < 1:
             if idx_in_lijst % 2 == 0:
-                lange_pauze_opties = blokken_links_laatste_halfuur_3u
+                lange_pauze_opties = blokken_links_laatste_uur_3u
             else:
-                lange_pauze_opties = blokken_rechts_laatste_halfuur_3u
+                lange_pauze_opties = blokken_rechts_laatste_uur_3u
         # Als er nog steeds te weinig zijn, neem alle blokken
         if len(lange_pauze_opties) < 1:
             for i in range(len(uur_col_pairs)-1):
