980ce6d1e4869eeba5e465390ae0c4fa1eb33f9e
Saar-creator <dayerssaar@gmail.com>
2025-09-14 22:25:38 +0200

    Update allowed time slots for breaks in planning

--- Planningsapp.py
+++ Planningsapp.py
@@ -1304,12 +1304,13 @@ for s in random.sample(lange_werkers, len(lange_werkers)):
         if not plaats_student(s, harde_mode=False):
             niet_geplaatst.append(s)
         continue
-    # Alleen de eerste drie halve uren (indices 0,1,2) zijn toegestaan
+    # Alleen de eerste 12 kwartieren (indices 0 t/m 11) zijn toegestaan voor lange pauzes
     halve_uren = []  # lijst van (col1, col2, uur1, uur2, pv, pv_row)
     werk_uren_set = set(werk_uren)
     verboden_uren = {werk_uren[0], werk_uren[-1]} if len(werk_uren) > 2 else set(werk_uren)
+    max_start_idx = min(11, len(pauze_cols)-2)  # idx 0 t/m 10 zijn halve uren binnen eerste 12 kwartieren
     for pv, pv_row in pv_rows:
-        for idx in range(min(3, len(pauze_cols)-1)):
+        for idx in range(max_start_idx+1):
             col1 = pauze_cols[idx]
             col2 = pauze_cols[idx+1]
             col1_header = ws_pauze.cell(1, col1).value
@@ -1919,9 +1920,10 @@ for pv, pv_row in pv_rows:
     naam = pv["naam"]
     werk_uren = get_student_work_hours(naam)
     if len(werk_uren) > 6:
-        # Alleen de eerste drie halve uren (indices 0,1,2) zijn toegestaan
+        # Alleen de eerste 12 kwartieren (indices 0 t/m 11) zijn toegestaan voor lange pauzes
         halve_uren = []  # lijst van (idx, col1, col2)
-        for idx in range(min(3, len(pauze_cols)-1)):
+        max_start_idx = min(11, len(pauze_cols)-2)  # idx 0 t/m 10 zijn halve uren binnen eerste 12 kwartieren
+        for idx in range(max_start_idx+1):
             col1 = pauze_cols[idx]
             col2 = pauze_cols[idx+1]
             cel1 = ws_pauze.cell(pv_row, col1)
